2c48272911c582b224aaaf93453c768399d26fb1
valet <valet@DESKTOP-R4K5LBG>
2022-10-23 14:00:23 +0200

    Ajout des constantes

---
+++ include/config.php
@@ -0,0 +1,1 @@
+<?php const NUM = '0474354562'; const MAIL ='jules.valette2@etu.univ-lyon1.fr'; const ADRESS = '9 Rue Pierre de Coubertin,01250 Ceyzériat';
--- modules/bottom.php
+++ modules/bottom.php
@@ -1,2 +1,4 @@
-<?php include('include/const.php');?>
-<p> Numéro : <?php echo(NUM);?></p>
+<?php include('include/config.php'); ?>
+<p> Numéro : <?php echo (NUM); ?></p>
+<p> Email : <a href='mailto:<?php echo (MAIL); ?>'><?php echo (MAIL); ?></a></p>
+<p> Adresse : <?php echo (ADRESS); ?></p>
